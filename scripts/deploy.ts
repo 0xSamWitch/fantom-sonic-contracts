8a4648b42651350aeb7c0862087db3ba39951840
0xSamWitch <julian.assange12@outlook.com>
2023-10-26 19:11:02 +0100

    Add Multicall contract, necessary when deployed on networks without it for libraries like viem

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,6 +6,11 @@ async function main() {
   const network = await ethers.provider.getNetwork();
   console.log(`Deploying contracts with the account: ${owner.address} on chain id: ${network.chainId}`);
   await deployERC1155();
+
+  // Deploy multicall if required
+  //  const multicall3 = await ethers.deployContract("Multicall3");
+  //  await multicall3.waitForDeployment();
+  //  console.log(`Multicall3 deployed to: ${await multicall3.getAddress()}`);
 }
 
 main().catch((error) => {
